c5d3bb303f38f47d04a9fa318dfaf7563b592693
Randy Bravo <rbroh99@gmail.com>
2024-05-13 15:48:49 -0400

    Fixed Django Admin password updating issue on apis sync

--- app/core/rabbitmq.py
+++ app/core/rabbitmq.py
@@ -6,6 +6,7 @@ import logging
 import asyncio
 import json
 
+from django.http import Http404
 
 from django.contrib.auth import get_user_model
 
@@ -30,27 +31,71 @@ def create_user(user_info):
         )
 
 
+def update_user(user_info):
+    """Syncronusly updates a existing user."""
+    try:
+        user = get_user_model().objects.get(id=user_info["id"])
+    except get_user_model().DoesNotExist:
+        raise Http404("Item does not exist")
+
+    password = user_info.pop("password", None)
+    email = user_info.get("email", None)
+
+    if password:
+        user.password = password
+    if email!=user.email:
+        user.email = email
+
+    user.save()
+
+
 create_user_async = sync_to_async(
     create_user,
     thread_sensitive=True)
 
 
+update_user_async = sync_to_async(
+    update_user,
+    thread_sensitive=True)
+
+
 async def start_consuming():
-    """Starts de RabbitMQ consumer with aio-pika."""
+    """Starts consuming from both 'user_created' and 'user_modified' queues."""
     connection = await aio_pika.connect_robust(RABBITMQ_URL)
     channel = await connection.channel()
 
-    queue = await channel.declare_queue('user_created', durable=True)
+    user_created_queue = await channel.declare_queue('user_created', durable=True)
+    user_modified_queue = await channel.declare_queue('user_modified', durable=True)
 
-    async with queue.iterator() as queue_iter:
-        async for message in queue_iter:
-            async with message.process():
-                user_info = message.body.decode()
-                user_info = json.loads(user_info)
-                print(f" MAIN API Received {user_info['email']}.")
-                await create_user_async(user_info)
-                print(f" User {user_info['email']} created")
+    user_created_iter = user_created_queue.iterator()
+    user_modified_iter = user_modified_queue.iterator()
 
+    async def process_user_created():
+        async with user_created_iter:
+            async for message in user_created_iter:
+                async with message.process():
+                    user_info = message.body.decode()
+                    user_info = json.loads(user_info)
+                    print(f"MAIN API Received {user_info['email']}.")
+                    await create_user_async(user_info)
+                    print(f"User {user_info['email']} created")
+
+    async def process_user_modified():
+        async with user_modified_iter:
+            async for message in user_modified_iter:
+                async with message.process():
+                    user_info = message.body.decode()
+                    user_info = json.loads(user_info)
+                    print(f"MAIN API Received {user_info['email']}.")
+                    await update_user_async(user_info)
+                    print(f"User {user_info['email']} updated")
+
+    await asyncio.gather(
+        process_user_created(),
+        process_user_modified()
+    )
+
+    await connection.close()
 
 async def secure_start_consuming():
     """Try to start RabbitMQ consumer handling errors."""
